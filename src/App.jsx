a99477916b7b580b468b39776d79c499b107689c
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-31 06:55:36 +0530

    latest commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import './index.css'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-5xl text-orange-500 font-bold'>
+        Welcome to the App
+      </h1>
+    </main>
+  )
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        zentry: ['zentery', 'sans-serif'],
+        general: ['general', 'sans-serif'],
+        'circular-web': ['circular-web', 'sans-serif'],
+        'robert-medium': ['robert-medium', 'sans-serif'],
+        'robert-regular': ['robert-regular', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
