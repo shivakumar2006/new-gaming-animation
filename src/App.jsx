aa986c6fc3cbd99cc9142240132204815a57ef78
shivakumar2006 <official.shivakumar06@gmail.com>
2025-02-01 07:51:13 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import About from './components/About';
 import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
-const App = () => {
+const App = () => { //// bg-zinc-600
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <main className='relative min-h-screen w-screen overflow-x-hidden'> 
+      <Navbar />
       <Hero />
-
       <About />
     </main>
   )
