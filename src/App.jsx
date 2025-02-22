23fc7f3767b50bb06957ef7a96dedc686a82be49
shivakumar2006 <official.shivakumar06@gmail.com>
2025-02-22 21:28:01 +0530

    add react lazy component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import About from './components/About';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-import Features from './components/Features';
-import Story from './components/Story';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import React, { Suspense } from 'react';
+
+const About = React.lazy(() => import("./components/About"));
+const Hero = React.lazy(() => import("./components/Hero"));
+const Navbar = React.lazy(() => import("./components/Navbar"));
+const Features = React.lazy(() => import("./components/Features"));
+const Story = React.lazy(() => import("./components/Story"));
+const Contact = React.lazy(() => import("./components/Contact"));
+const Footer = React.lazy(() => import("./components/Footer"));
 
 const App = () => { //// bg-zinc-600
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <main className='relative min-h-screen w-screen overflow-x-hidden'> 
       <Navbar />
       <Hero />
@@ -17,6 +20,7 @@ const App = () => { //// bg-zinc-600
       <Contact />
       <Footer />
     </main>
+    </Suspense>
   )
 }
 
